cd656acb92eafdb0c2573b314e9987bee39f4a71
Marcin Trebacz <emteweb@gmail.com>
2020-10-23 20:01:17 +0200

    CRUD - editing a product

--- ProductApp/index.js
+++ ProductApp/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
 const Product = require('./Models/product');
+const { Console } = require("console");
 
 mongoose.connect('mongodb://localhost:27017/productApp', {useNewUrlParser: true})
         .then(() => {
@@ -15,13 +18,21 @@ mongoose.connect('mongodb://localhost:27017/productApp', {useNewUrlParser: true}
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.post('/products', async(req, res) => {
     const newProduct =  new Product(req.body);
     await newProduct.save();
     console.log(newProduct);
     res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('Products/show', { product });
 });
 
 app.get('/products', async (req, res) => {
@@ -33,10 +44,16 @@ app.get('/products/new', (req, res) => {
     res.render('Products/new')
 });
 
-app.get('/products/:id', async (req, res) => {
+app.get('/products/:id/edit', async (req, res) => {
     const {id} = req.params;
     const product = await Product.findById(id);
-    res.render('Products/show', { product });
+    res.render('Products/edit', {product})
 });
+
+app.put('/products/:id', async(req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/products/${product._id}`);
+})
 
 app.listen(process.env.PORT || 3000, () => console.log('ProductApp listening on port 3000!'));
