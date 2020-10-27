669587e38ba3e8d959cb5147dc26a07be0389d34
Marcin Trebacz <emteweb@gmail.com>
2020-10-27 20:38:18 +0100

    fixed app

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+const Product = require('./Models/product');
+
+mongoose.connect('mongodb://localhost:27017/productApp', {useNewUrlParser: true})
+        .then(() => {
+            console.log("MONGO Connection OPEN")
+        })
+        .catch(err => { 
+            console.log("Connection Error")
+            console.log(err)
+        });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: true}));
+
+// template for adding a new product
+app.get('/products/new', (req, res) => {
+    res.render('Products/new')
+}); 
+
+// adding a new product
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('Products/edit', {product});
+}); 
+
+app.put('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`);
+    
+})
+
+// displaying detail of a product
+app.get('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('Products/show', { product });
+});
+
+// displaying all products
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('Products/index', {products})
+});
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => console.log('ProductApp listening on port 3000!'));
